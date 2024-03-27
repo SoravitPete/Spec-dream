da22753abba3f9692fc33a2f624be2d94efe4bcf
SoravitPete <esterpiano.official@gmail.com>
2024-03-27 12:08:21 +0700

    fix git

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EvolutionForm from './component/EvolutionForm'; // Path adjusted to reflect the new location
+import './App.css';
+
+function App() {
+  return ( 
+    <div className="App">
+      <h1>PC Builder</h1>
+      <EvolutionForm />
+    </div>
+  );
+}
+
+export default App;
